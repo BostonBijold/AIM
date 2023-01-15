f74e66e9aeff828528f90b216ed5ddfc57a8b237
BossBijold <bostonrbijold@gmail.com>
2023-01-15 12:19:39 -0700

    goal UI update

--- components/TasksOutput.js
+++ components/TasksOutput.js
@@ -2,33 +2,6 @@ import { StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../constants/colors";
 import TaskList from "../storage/TaskList";
 
-const DUMMY_TASKS = [
-  {
-    id: "t1",
-    goalId: "g1",
-    description: "Learn React Native.",
-    isComplete: false,
-  },
-  {
-    id: "t2",
-    goalId: "g1",
-    description: "Learn source control in bitbucket.",
-    isComplete: false,
-  },
-  {
-    id: "t3",
-    goalId: "g1",
-    description: "Add a focus screen.",
-    isComplete: false,
-  },
-  {
-    id: "t4",
-    goalId: "g2",
-    description: "Plan a pant-less weekend.",
-    isComplete: false,
-  },
-];
-
 function TasksOutput({ tasks }) {
   return (
     <View style={styles.container}>
